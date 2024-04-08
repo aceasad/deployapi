569de837105241e614f54bf7b4bf6d5645bf5ebf
Asad Saeed <asad.saeed@live.com>
2024-04-08 22:37:07 +0500

    updated

--- redmarble.py
+++ redmarble.py
@@ -192,5 +192,5 @@ def train():
     return jsonify({'message': 'Model trained and saved successfully.'})
 
 if __name__ == '__main__':
-    app.run(port=80, debug=True, host='0.0.0.0')
+    app.run(port=5000, debug=True, host='0.0.0.0')
 
